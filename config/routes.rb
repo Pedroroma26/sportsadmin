1a05bde3287dee65fceacd085cd4212563aefbbb
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-07 15:49:52 +0000

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#dashboard"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :clubs, only: [:index, :show] do
+    resources :players, only: [:new, :create]
+  end
+  resources :games do
+    resources :reports, only: [:new, :show, :create]
+  end
+  resources :reports, only: [:index]
+  resources :competitions, only: [:index, :show]
+  resources :referees, only: [:index, :edit, :update]
 end

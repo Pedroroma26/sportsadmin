7a2d47102294c26605b7315d16cd09314d76d2a2
rui silva <ruimorimsilva@gmail.com>
2023-03-07 17:45:53 +0000

    players controller and view is working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,14 +1,16 @@
 class PlayersController < ApplicationController
-  
+  before_action :set_club
+
   def new
     @player = Player.new
   end
 
   def create
     @player = Player.new(player_params)
+    @player.club = @club
 
     if @player.save
-      redirect_to club_players_path, notice: 'player created'
+      redirect_to club_path(@club), notice: 'player created'
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,7 +18,11 @@ class PlayersController < ApplicationController
 
   private
 
+  def set_club
+    @club = Club.find(params[:club_id])
+  end
+
   def player_params
-    params.require(:player).permit(:first_name, :last_name, :birth_date, :gender, :club_id)
+    params.require(:player).permit(:first_name, :last_name, :birth_date, :gender)
   end
 end

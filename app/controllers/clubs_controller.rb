498cc72f7da80e7f91cf153c7c21a32b0352677a
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-12 14:30:48 +0000

    buttons delete edit clubs

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -4,9 +4,35 @@ class ClubsController < ApplicationController
     @clubs = Club.all
   end
 
+  def new
+    @club = Club.new
+  end
+
   def show
     @club = Club.find(params[:id])
   end
+
+  def create
+    @club = Club.new(club_params)
+
+    if @club.save
+      redirect_to clubs_path, notice: "Club created successfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @club = Club.find(params[:id])
+  end
+
+  def destroy
+    @club = Club.find(params[:id])
+    @club.destroy
+
+    redirect_to clubs_path, notice: "Club deleted successfully"
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#dashboard"
 
-  resources :clubs, only: [:index, :show] do
+  resources :clubs do
     resources :players, only: [:new, :create]
   end
   resources :games, except: [:new, :create] do

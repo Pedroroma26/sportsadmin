55407483262db2158fef4101f244efbcc3f11081
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-13 21:55:12 +0000

    done

--- app/models/club.rb
+++ app/models/club.rb
@@ -8,6 +8,6 @@ class Club < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :abbreviation, presence: true, length: { maximum: 30 }
   validates :address, presence: true
-  validates :email, presence: true, uniqueness: true, confirmation: true
+  validates :email, presence: true, uniqueness: true, confirmation: true # serve para confirmar o email
   validates :phone_number, length: { maximum: 15 }
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,6 +1,6 @@
 class Competition < ApplicationRecord
   acts_as_paranoid
-  
+
   has_many :games, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true

d90751f298b6bd1b04b92c078b59fe32129a2964
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-13 09:35:08 +0000

    seeds corrected

--- db/seeds.rb
+++ db/seeds.rb
@@ -88,7 +88,7 @@ puts "club6 created"
 # lisboncasuals
 club7 = Club.new(name: "Lisbon Casuals Hockey Club", abbreviation: "Lisbon Casuals HC", address: "Centro Desportivo Nacional do Jamor", email: "emasfafaail@email.lw", phone_number: "123456789")
 # lisboncasuals image
-file = URI.open("https://res.cloudinary.com/diey6169r/image/upload/v1678273994/Sports%20Admin/juventudehoqueiclube_tsneqp.png")
+file = URI.open("https://res.cloudinary.com/diey6169r/image/upload/v1678273994/Sports%20Admin/lisboncasuals_awq9sh.png")
 club7.photo.attach(io: file, filename: "lisboncasuals.png", content_type: "image/png")
 club7.save
 puts "club7 created"

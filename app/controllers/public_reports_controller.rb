0c4387d6750628094b4eafdab3daa5f03388e193
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-14 18:45:26 +0000

    almost done3

--- app/controllers/public_reports_controller.rb
+++ app/controllers/public_reports_controller.rb
@@ -1,8 +1,18 @@
 class PublicReportsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
+
   layout "public"
 
   def show
     @report = Report.find(params[:id])
+    @club = Club.find_by(access_token: params[:token])
+
+    if @club.nil?
+      redirect_to root_path
+    elsif @report.nil?
+      redirect_to root_path
+    elsif @report.include(@club)
+      redirect_to root_path
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_115022) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_164847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_115022) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.datetime "deleted_at"
+    t.string "access_token"
     t.index ["deleted_at"], name: "index_clubs_on_deleted_at"
   end
 
